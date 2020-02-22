3f57d8ffc7b100146cff434fa8d95c7d975d9e18
mehtabhavin10 <34479190+mehtabhavin10@users.noreply.github.com>
2020-02-22 15:15:10 +0530

    Category endpoint added

--- analytics/views.py
+++ analytics/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render, HttpResponse
 def index(request):
     context_data = {}
     return render(request, 'dashboard.html', context_data)
+
+
+def setCategory(request):
+    category = request.POST.category;
+    print(category)
+    context_data = {}
+    return render(request, 'dashboard.html', context_data)
